49efca32c688b21efa76e8595828b783d3043868
Ádám Horváth <horvath.adam@ecoform.hu>
2023-12-12 10:18:19 +0100

    Optimized first solution

--- First_Day/AoC1_1.py
+++ First_Day/AoC1_1.py
@@ -1,18 +1,12 @@
-with open('first_puzzle.txt', 'r') as file:
+with open('First_Day\\first_puzzle.txt', 'r') as file:
     answer = 0
     linenumber = 1
-    for line in file.readlines():
-        first_digit = '-1'
-        last_digit = '-1'
-        for c in line:
-            if ((first_digit == '-1') & c.isdigit()):
-                first_digit = c
-            if (c.isdigit()):
-                last_digit = c
-        last_digit_normalized = 0
-        if (int(last_digit) >= 0):
-            last_digit_normalized = int(last_digit)
-        answer += (int(first_digit) * 10 + last_digit_normalized)
+    for line in file:
+        digits = [c for c in line if c.isdigit()]
+        if digits:
+            first_digit = int(digits[0])
+            last_digit = int(digits[-1])
+            answer += (first_digit * 10 + last_digit)
         print(first_digit, last_digit, linenumber)
         linenumber += 1
 print(answer)
